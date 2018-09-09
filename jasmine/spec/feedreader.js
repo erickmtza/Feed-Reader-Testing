946eac0484215f534e9811f9ccd0eaa03ba6e2fe
Erick Martinez <777areri@gmail.com>
2018-09-09 00:11:28 -0500

    Test ensures new feed is loaded & updates content

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -98,11 +98,21 @@ $(function() {
     /* TODO: Write a new test suite named "New Feed Selection" */
     describe('New Feed Selection', function() {
 
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-         */
-
-    });
-
+              beforeEach(function(done) {
+                loadFeed(0, function() {
+                  firstEntry = $('.feed').find(allFeeds.url);
+                });
+                loadFeed(1, function() {
+                  secondEntry = $('.feed').find(allFeeds.url);
+                  done();
+                });
+              })
+              /* TODO: Write a test that ensures when a new feed is loaded
+               * by the loadFeed function that the content actually changes.
+               * Remember, loadFeed() is asynchronous.
+               */
+               it('updates content', function() {
+                 expect(firstEntry).not.toBe(secondEntry);
+               })
+          });
 }());
