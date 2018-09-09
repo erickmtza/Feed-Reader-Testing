db1434f13925f01ff093347d8f216df6d43954c4
Erick Martinez <777areri@gmail.com>
2018-09-08 21:14:06 -0500

    Use beforeEach in suite for async function

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -80,7 +80,10 @@ $(function() {
     /* TODO: Write a new test suite named "Initial Entries" */
     describe('Initial Entries', function() {
 
-
+        beforeEach(function(done) {
+          loadFeed(0);
+          done();
+        })
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
